40a9c8632ddefb56b49b9859a735b3f7a6128b92
Peter Lee <609866@qq.com>
2019-07-12 10:40:56 +0800

    Initial commit

---
+++ Assets/Scripts/TestXlua.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class TestXlua : MonoBehaviour
+{
+    LuaEnv luaenv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        luaenv = new LuaEnv();
+        TextAsset ta = Resources.Load<TextAsset>("main.lua");
+        print(ta);
+        luaenv.DoString(ta.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnDestroy()
+    {
+        luaenv.Dispose();
+    }
+}
