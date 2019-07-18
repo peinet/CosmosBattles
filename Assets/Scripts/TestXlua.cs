be68590938888fdf65f82fa131c2b0c8653e27f0
chens <bighead3827@163.com>
2019-07-18 11:56:39 +0800

    IDEA182+EMMYLUA182配置

--- Assets/Scripts/TestXlua.cs
+++ Assets/Scripts/TestXlua.cs
@@ -9,9 +9,22 @@ public class TestXlua : MonoBehaviour
     void Start()
     {
         luaenv = new LuaEnv();
-        TextAsset ta = Resources.Load<TextAsset>("main.lua");
+        luaenv.AddLoader((ref string filename) =>
+        {
+            if (filename == "Luas/XXX/XXX")
+            {
+                UnityEngine.Debug.Log("test decode");
+                //加载文件
+                //解密文件
+                //luaenv.DoString("文件内容");
+                //return System.Text.Encoding.UTF8.GetBytes("文件内容");
+            }
+            return null;
+        });
+        TextAsset ta = Resources.Load<TextAsset>("Luas/main.lua");
         print(ta);
-        luaenv.DoString(ta.text);
+        luaenv.DoString(ta.text, "main.lua");
+
     }
 
     // Update is called once per frame
